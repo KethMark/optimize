2e1a611bd165821cd430f93ea8bed25fe543dc69
Keth Mark <aseronberou@gmail.com>
2025-02-05 11:26:26 +0800

    ...

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
   serverExternalPackages: ['sharp', 'onnxruntime-node'],
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; 
+    return config;
+  },
 };
 
 export default nextConfig;
